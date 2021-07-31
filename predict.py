daf2888193c3312d41e11b723cbd8af8a800fd9a
maroacc <maroamedina@gmail.com>
2021-07-31 13:56:54 +0200

    Added results.py

--- predict.py
+++ predict.py
@@ -82,8 +82,7 @@ def predict(data_type, seq_length, model, saved_model=None,
     print(predicted_labels)
     results = Results(class_indices, dataset_name=dataset_name)
 
-    #accuracy, confusion_matrix, classification = results.compute(test_generator.filenames, test_generator.classes,
-                                                                 predicted_labels)
+    #accuracy, confusion_matrix, classification = results.compute(test_generator.filenames, test_generator.classes,predicted_labels)
     # Display and save results
     #results.print(accuracy, confusion_matrix)
 
