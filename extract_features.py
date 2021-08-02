af3bc3dd07063cfaf74a3d111e5b920fb88b59da
maroacc <maroamedina@gmail.com>
2021-08-02 16:25:02 +0200

    Can predict validation set

--- extract_features.py
+++ extract_features.py
@@ -26,14 +26,9 @@ def extract_features(seq_length=40, class_limit=2, image_shape=(299, 299, 3), pr
     pbar = tqdm(total=len(data.data))
     for video in data.data:
 
-        if predict:
-            # Get the path to the sequence for this video.
-            path = os.path.join('/content/drive/MyDrive/cnn/predict/data', 'sequences', video[2] + '-' + str(seq_length) + \
-                '-features')  # numpy will auto-append .npy
-        else:
-            # Get the path to the sequence for this video.
-            path = os.path.join('/content/data', 'sequences', video[2] + '-' + str(seq_length) + \
-                                '-features')  # numpy will auto-append .npy
+        # Get the path to the sequence for this video.
+        path = os.path.join('/content/data', 'sequences', video[2] + '-' + str(seq_length) + \
+                            '-features')  # numpy will auto-append .npy
 
         # Check if we already have it.
         if os.path.isfile(path + '.npy'):
